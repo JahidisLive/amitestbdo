4cd709e360f73dbe628486cde4abe10e5db473ca
JahidisLive <67954674+JahidisLive@users.noreply.github.com>
2021-02-24 18:21:52 +0600

    Add files via upload

---
+++ fb.js
@@ -0,0 +1,206 @@
+// Auto Love React //
+
+
+javascript:
+var btns = document.getElementsByClassName("_1ekf"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Show more reactions") {
+  btns[i].click();
+var n = document.getElementsByClassName("img _mpx _4sue img _2sxw _4s0y");
+  if(n[1].textContent=="Love");
+  n[1].click();
+ }
+ i++;
+ },1000);
+
+
+// Auto Like with Browse //
+
+
+javascript:
+var btns = document.getElementsByClassName("_15ko _77li touchable"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Like") {
+  btns[i].click();
+ }
+ i++;
+ },800);
+ 
+
+
+// Comment Like //
+
+
+javascript:
+var btns = document.getElementsByClassName("touchable _2b0a"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Like") {
+  btns[i].click();
+ }
+ i++;
+ },500);
+
+
+
+
+// Add Suggest / Friends of Friend //
+
+
+javascript:
+var btns = document.getElementsByClassName("_54k8 _52jg _56bs _26vk _40x9 _8yzt _56bu"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Add Friend") {
+  btns[i].click();
+ }
+ i++;
+ },1000);
+
+
+javascript:
+var btns = document.getElementsByClassName("_55sr"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Add Friend") {
+  btns[i].click();
+ }
+ i++;
+ },1000);
+
+
+// Remove Suggest Friends //
+
+
+javascript:
+var btns = document.getElementsByClassName("_54k8 _56bs _26vk _56b_ _54k7 _8yzn _8yo0 _56bt _52jg"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Remove") {
+  btns[i].click();
+ }
+ i++;
+ },500);
+
+
+// Cancel All Send Request //
+
+javascript:var scroll = setInterval(function(){ window.scrollBy(0,500); }, 1000);var btns = document.getElementsByClassName("_54k8 _56bs _56bt");var i=0; setInterval(function(){  if (btns[i].textContent=="Cancel") {   btns[i].click();  }  i++;  },500);
+ 
+
+
+
+
+javascript:
+var inputs = document.getElementsByClassName('_54k8 _56bs _56bt'); 
+for(var i=0; i<inputs.length;i++) 
+	{ 
+		inputs[i].click();
+		 }
+
+
+// Confirm Friends Request //
+
+javascript:
+var btns = document.getElementsByClassName("_54k8 _52jg _56bs _26vk _56b_ _3cqr _5uc2 _8yo0 _56bu"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Confirm") {
+  btns[i].click();
+ }
+ i++;
+ },500);
+
+
+// Delete Friends Request //
+
+javascript:
+var btns = document.getElementsByClassName("_54k8 _52jg _56bs _26vk _56b_ _5uc3 _3cqr _8yo0 _56bt"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Delete") {
+  btns[i].click();
+ }
+ i++;
+ },500);
+
+
+// Hide Friends Request //
+
+javascript:
+var btns = document.getElementsByClassName("_54k8 _56bs _26vk _56bt _52jg"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Mark as Spam") {
+  btns[i].click();
+ }
+ i++;
+ },500);
+
+
+// Add Liker or Reactor Friends //
+
+
+javascript:
+var btns = document.getElementsByClassName("_54k8 _52jg _56bs _26vk _8yzt _56bu"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Add Friend") {
+  btns[i].click();
+ }
+ i++;
+ },1000);
+var scroll = setInterval(function(){ window.scrollBy(0,60); }, 1000);
+
+
+
+
+
+javascript:
+var btns = document.getElementsByClassName("_54k8 _52jg _56bs _26vk _8yzt _56bu"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Add Friend") {
+  btns[i].click();
+ }
+ i++;
+ },1000);
+
+
+
+// Unfriend All Friends //
+
+
+javascript:
+var btns = document.getElementsByClassName("_54k8 _52jg _56bs _26vk _3cqr _8yzo _8yo0 _56bt"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Friends") {
+  btns[i].click();
+var n = document.getElementsByClassName("_54k8 _55i1 _58a0 touchable");
+  if(n[1].textContent=="Unfriend");
+  n[1].click();
+ }
+ i++;
+ },1000);
+
+
+javascript:
+var btns = document.getElementsByClassName("_54k8 _52jg _56bs _26vk _2b4n _56bt"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="Friends") {
+  btns[i].click();
+var n = document.getElementsByClassName("_54k8 _55i1 _58a0 touchable");
+  if(n[1].textContent=="Unfriend");
+  n[1].click();
+ }
+ i++;
+ },1000);
+ 
+
+javascript:
+var btns = document.getElementsByClassName("img _71t _8yzt img _2sxw"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="") {
+  btns[i].click();
+var n = document.getElementsByClassName("_54k8 _55i1 _58a0 touchable");
+  if(n[1].textContent=="Unfriend");
+  n[1].click();
+ }
+ i++;
+ },1000);
+ 
+ 
+  // unfollow all //
+
+javascript:
+var btns = document.getElementsByClassName("img _2hu2 profpic"); var i=0; setInterval(function(){
+ if (btns[i].textContent=="") {
+  btns[i].click();
+ }
+ i++;
+ },1000);
+ 
+ 
+ 
+ // Auto Scroll //
+ 
+javascript:
+ var scroll = setInterval(function(){ window.scrollBy(0,1000); }, 800);
+ 
